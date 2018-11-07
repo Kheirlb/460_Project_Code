df671f5a704074b9c121f1fe654c6577c6999ddd
Karl Parks <kheirlbparks@gmail.com>
2018-11-07 10:19:25 -0800

    Update Lecture 11.7.18/main.m

---
+++ Lecture 11.7.18/main.m
@@ -0,0 +1,14 @@
+w_not = rand (3,1);
+q_not = rand (4,1); q_not = q_not/norm(q_not);
+t = [0, 30];
+I = diag([0.1,0.2,0.3]);
+[t,x] = ode45(....
+
+
+%This is PID Concept
+
+%Optimal Control Concept
+
+%Autotuning Gains
+
+%Feedback Sensitivities
