a02ede47fed0cf5b92e80d1944312ddfed9df2b8
Karl Parks <kheirlbparks@gmail.com>
2018-11-26 11:00:59 -0800

    Changes by younes

--- Attitude/attFunc.m
+++ Attitude/attFunc.m
@@ -6,7 +6,7 @@ w = x(1:3);
 q = x(4:7);
 u = -k1*w-k2*q(1:3);
 w_dot=I\(-cross(w,I*w)+u);
-q_dot = 0.5*[-tilde(w), w; w.', 0]*q;
+q_dot = 0.5*[-tilde(w), w; -w', 0]*q;
 x_dot = [w_dot;q_dot];
 
 end
--- Attitude/mainAttitude.m
+++ Attitude/mainAttitude.m
@@ -1,4 +1,5 @@
 %mainAttitude
+clear;clc;close all;
 global I;
 global k1;
 global k2;
@@ -6,8 +7,20 @@ w_not = rand(3,1);
 q_not = rand(4,1);
 q_not = q_not/norm(q_not);
 t = [0,30];
-k1 = 1;
-k2 = 2;
+k1 = 0.5;
+k2 = 0.5;
 I = diag([0.1,0.2,0.3]);
 [t,X] = ode45(@attFunc, t, [w_not;q_not]);
-plot(X);
+
+% calculate the control input for plotting purposes;
+for i=1:length(t)
+    u(i,:) = -k1*X(i,1:3)-k2*X(i,4:6);
+end
+
+figure; subplot(3,1,1); plot(t,X(:,1:3));grid;legend('\omega_x','\omega_y','\omega_z');
+ylabel('angular velocity [rad/sec]');
+subplot(3,1,2); plot(t,X(:,4:end));grid;legend('q_1','q_2','q_3','q_4');
+ylabel('Quaternion q(t)')
+subplot(3,1,3); plot(t,u(:,1:3));grid;legend('u_1','u_2','u_3');
+xlabel('time [sec]');
+ylabel('control input u(t)')
